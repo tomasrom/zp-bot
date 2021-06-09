ddc078e0e6ae6ff4b26b0e01830c40c792156be5
Juan Barneix <juanbarcruz96@gmail.com>
2021-06-09 18:40:39 -0300

    extract links method & and pip configs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # zp-bot
+
 Bot para analizar alquileres.
+
+
+# PIPENV : https://pypi.org/project/pipenv/
+
+##Ahora el proyecto esta guardado en un entorno virtual. Esto nos permite ( entre otras cosas ) instalar librerias dentro del proyecto y no en nuestro sistema.
+##Para correr el proyecto se usa el comando (en la terminal) `pipenv shell`
+##Antes de hacerlo, ejecutar `pipenv install` para instalar las librerias que se usan en el proyecto, dentro del entorno virtual. Para instalar una libreria nueva, y que se guarde en el Pipfile usar el comando `pipenv install <nombre de la libreria>`
--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-def main():
-    print("1")
+from parser import Parser
 
+
+def visto(link):
+    return False
+
+
+def no_vistos(links):
+    return True
+
+
+def main():
+    argenprop = Parser(
+            website='https://www.argenprop.com/departamento-alquiler-localidad-capital-federal', # noqa
+            soup_tag='a',
+            next_page='-pagina-',
+            starts_with='/departamento-en-alquiler',
+            page_limit=10,
+            )
+    no_vistos(argenprop.extract_links())
+    # for testing :
+    # links = argenprop.extract_links()
+    # print(links)
+
+
+main()
+
+
+# uncomment for test with the interactive shell ( python -i main.py )
+# argenprop = Parser(
+#                website='https://www.argenprop.com/departamento-alquiler-localidad-capital-federal', # noqa
+#                soup_tag='a',
+#                next_page='-pagina-',
+#                starts_with='/departamento-en-alquiler',
+#                page_limit=2,
+#            )
